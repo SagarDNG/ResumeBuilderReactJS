90d0acfffa46c96cbf03e375ba027093a96e17c4
SagarDNG <sagardng123@gmail.com>
2022-06-26 16:18:49 +0530

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import Resume from "../../assets/Resume.svg";
+
+import styles from "./Header.module.css";
+
+function Header() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <p className={styles.heading}>
+          A <span>Resume</span> that stands out!
+        </p>
+        <p className={styles.heading}>
+          Make your own resume. <span>It's free</span>
+        </p>
+      </div>
+      <div className={styles.right}>
+        <img src={Resume} alt="Resume SVG" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
