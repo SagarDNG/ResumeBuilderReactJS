191aee644d0e50c6ab7fb458956cbdfa5a4401fd
SagarDNG <sagardng123@gmail.com>
2022-06-26 19:35:32 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from "./components/Header/Header";
 import Body from "./components/Body/Body";
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Body />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+import styles from "./Footer.module.css"
+
+function Footer() {
+    return (
+        <div className={styles.container}>
+            Copyright © 2022, All rights reserved <br />
+            Made with ♥ by <a href="https://sagardng123.wixsite.com/sagardng">SagarDNG</a>
+        </div>
+    )
+}
+
+export default Footer
